d52ce22dfdb8a7d3ca30bf8666ea8614670c4503
Vincent Spiewak <vspiewak@gmail.com>
2013-09-14 13:49:18 +0200

    Add log parameter required

--- src/main/java/com/github/vspiewak/loggenerator/InputParameters.java
+++ src/main/java/com/github/vspiewak/loggenerator/InputParameters.java
@@ -4,13 +4,13 @@ import com.beust.jcommander.Parameter;
 
 public class InputParameters {
 
-    @Parameter(names = {"-logs", "-n"}, description = "Number of logs to generate")
-    public Long logs = 10L;
+    @Parameter(names = {"-logs", "-n"}, description = "Number of logs to generate", required = true)
+    public Long logs;
 
     @Parameter(names = {"-threads", "-t"}, description = "Number of threads to use")
     public Integer threads = 2;
 
     @Parameter(names = "--help", help = true)
-    private boolean help;
+    public boolean help;
 
 }
